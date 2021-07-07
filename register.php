5ea81d5f424f39ea5534b025e55cdc659887297a
Sean Oyler <xrysen@gmail.com>
2021-07-07 16:55:39 -0700

    Finishes logic for user registration

--- register.php
+++ register.php
@@ -32,5 +32,47 @@
         mysqli_stmt_close($stmt);
       }
     }
+
+    // Validate password
+    if(empty(trim($_POST["password"]))) {
+      $password_err = "Please enter a password";
+    } elseif (strlen(trim($_POST["password"])) < 6) {
+      $password_err = "Password must have at least 6 characters";
+    } else {
+      $password = trim($_POST["password"]);
+    }
+
+    // Validate confirm password
+    if(empty(trim($_POST["confirm_password"]))) {
+      $confirm_password_err = "Please confirm password";
+    } else {
+      $confirm_password = trim($_POST["confirm_password"]);
+      if(empty($password_err) && ($password != $confirm_password)) {
+        $confirm_password_err = "Passwords did not match";
+      }
+    }
+
+    // Check for input errors before inserting into database
+    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)) {
+      $sql = "INSERT INTO users (username, password) VALUES(?, ?)";
+
+      if($stmt = mysqli_prepare($link, $sql)) {
+        mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
+
+        // Set parameters
+        $param_username = $username;
+        $param_password = password_hash($password, PASSWORD_DEFAULT);
+
+        if (mysqli_stmt_execute($stmt)) {
+          // Redirect to login page
+          header("location: login.php");
+        } else {
+          echo "Oops! Something went wrong. Please try again later";
+        }
+
+        mysqli_stmt_close();
+      }
+    }
+    mysqli_close($link);
   }
 ?>
