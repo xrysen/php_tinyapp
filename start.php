503fdf2e5d14578d5759b02e69525adeccfbc169
Sean Oyler <xrysen@gmail.com>
2021-07-07 15:32:14 -0700

    brings in database connection in start.php

--- start.php
+++ start.php
@@ -1,8 +1,12 @@
 <?php
 require 'vendor/autoload.php';
 
+use Src\Database;
+
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
 
 echo $_ENV['DB_HOST'];
+
+$dbConnection = (new Database())->connect();
 ?>
