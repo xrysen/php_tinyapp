edf62b50b0f9761711338c6a95e66f7d6752c659
Sean Oyler <xrysen@gmail.com>
2021-07-08 14:49:59 -0700

    restarting

--- register.php
+++ register.php
@@ -1,119 +1,29 @@
-<?php
-  require_once "config.php";
-
-  // Define Variables and initialize with empty values"
-  $username = $password = $confirm_password = "";
-  $username_err = $password_err = $confirm_password_err = "";
-
-  if($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Validate username
-    if(empty(trim($_POST['username']))) {
-      $username_err = "Please enter a username";
-    } elseif(!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["username"]))) {
-      $username_err = "Username can only contain letters, numbers and underscores";
-    } else {
-      $sql = "SELECT id FROM users WHERE username = ?";
-
-      if ($stmt = mysqli_prepare($link, $sql)) {
-        mysqli_stmt_bind_param($stmt, "s", $param_username);
-
-        if ( mysqli_stmt_exectute($stmt)) {
-          mysqli_stmt_store_result($stmt);
-
-          if(mysqli_stmt_num_rows($stmt) == 1) {
-            $username_err = "This username is already taken";
-          } else {
-            $username = trim($_POST["username"]);
-          }
-        } else {
-          echo "Opps! Something went wrong. Please try again later";
-        }
-
-        mysqli_stmt_close($stmt);
-      }
-    }
-
-    // Validate password
-    if(empty(trim($_POST["password"]))) {
-      $password_err = "Please enter a password";
-    } elseif (strlen(trim($_POST["password"])) < 6) {
-      $password_err = "Password must have at least 6 characters";
-    } else {
-      $password = trim($_POST["password"]);
-    }
-
-    // Validate confirm password
-    if(empty(trim($_POST["confirm_password"]))) {
-      $confirm_password_err = "Please confirm password";
-    } else {
-      $confirm_password = trim($_POST["confirm_password"]);
-      if(empty($password_err) && ($password != $confirm_password)) {
-        $confirm_password_err = "Passwords did not match";
-      }
-    }
-
-    // Check for input errors before inserting into database
-    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)) {
-      $sql = "INSERT INTO users (username, password) VALUES(?, ?)";
-
-      if($stmt = mysqli_prepare($link, $sql)) {
-        mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
-
-        // Set parameters
-        $param_username = $username;
-        $param_password = password_hash($password, PASSWORD_DEFAULT);
-
-        if (mysqli_stmt_execute($stmt)) {
-          // Redirect to login page
-          header("location: login.php");
-        } else {
-          echo "Oops! Something went wrong. Please try again later";
-        }
-
-        mysqli_stmt_close();
-      }
-    }
-    mysqli_close($link);
-  }
-?>
+<?php include('server.php'); ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Sign Up</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <style>
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 360px; padding: 20px; }
-    </style>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <title>Register</title>
 </head>
 <body>
-    <div class="wrapper">
-        <h2>Sign Up</h2>
-        <p>Please fill this form to create an account.</p>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <div class="form-group">
-                <label>Username</label>
-                <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
-                <span class="invalid-feedback"><?php echo $username_err; ?></span>
-            </div>    
-            <div class="form-group">
-                <label>Password</label>
-                <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
-                <span class="invalid-feedback"><?php echo $password_err; ?></span>
-            </div>
-            <div class="form-group">
-                <label>Confirm Password</label>
-                <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>">
-                <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
-            </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Submit">
-                <input type="reset" class="btn btn-secondary ml-2" value="Reset">
-            </div>
-            <p>Already have an account? <a href="login.php">Login here</a>.</p>
-        </form>
-    </div>    
+  <header>
+    <h1>Register</h1>
+  </header>
+
+  <form method = "POST" action="register.php">
+    <?php include('errors.php'); ?>
+    <div class = "mb-3 row">
+      <label class = "form-label" for = "username">Username</label>
+      <input type="text" name = "username" class = "form-control" value="<?php echo $username; ?>">
+    </div>
+    <label for = "email">Email: </label>
+    <input type = "text" name = "email" value="<?php echo $email; ?>">
+  </form>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>  
 </body>
 </html>
---
+++ server.php
@@ -0,0 +1,4 @@
+<?php
+  $username = "";
+  $email = "";
+?>
