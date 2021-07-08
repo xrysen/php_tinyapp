799aff6ad90872ed75f70d39b4d878f857bbb87e
Sean Oyler <xrysen@gmail.com>
2021-07-08 15:41:31 -0700

    Fixes error where user wasn't being added to database

--- server.php
+++ server.php
@@ -5,34 +5,41 @@
   $email = "";
   $errors = array();
 
+  $conn = new mysqli('localhost', 'root', '', 'tinyapp');
 
-  $db = mysqli_connect('localhost', 'root', '', 'tinyapp');
-
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->$connect_error);
+  }
+  
   if(isset($_POST['reg_user'])) {
-    $email = mysqli_real_escape_string($db, $_POST['email']);
-    $password_1 = mysqli_real_escape_string($db, $_POST['password1']);
-    $password_2 = mysqli_real_escape_string($db, $_POST['password2']);
+    $email = $conn->real_escape_string($_POST['email']);
+    $password_1 = $conn->real_escape_string($_POST['password1']);
+    $password_2 = $conn->real_escape_string($_POST['password2']);
 
     if(empty($email))  { array_push($errors, 'Email is required'); } 
     if(empty($password_1)) { array_push($errors, 'Password is required'); }
     if($password_1 != $password_2) { array_push($errors, "Passwords don't match"); }
 
-    $email_check_query = "SELECT * FROM users WHERE email = '$email' LIMIT 1;";
+    $email_check_query = "SELECT * FROM users WHERE email = '$email';";
 
-    $result = mysqli_query($db, $email_check_query);
-    $user = mysqli_fetch_assoc($result);
+    $result = $conn->query($email_check_query);
 
-    if ($user) { 
-      array_push($errors, "A user with that e-mail already exists");
-    }
+    // if ($result->num_rows > 0) { 
+    //   array_push($errors, "A user with that e-mail already exists");
+    // }
 
     if (count($errors) == 0) {
       $password = md5($password_1); // Encrypt password
-      $query = "INSERT INTO users (email, password) VALUES ('$email', '$password');";
-      $mysqli_query($db, $query);
+      $query = "INSERT INTO users (name, password) VALUES ('$email', '$password');";
+      if ($conn->query($query) === TRUE) {
+        echo "User successfully added!";
+      } else {
+        echo "Error ". $query . "<br>" . $conn->error;
+      }
       $_SESSION['username'] = $email;
       $_SESSION['success'] = "You are now logged in";
-      header("location: index.php");
+      //header("location: index.php");
+      $conn->close();
     }
   }
 ?>
